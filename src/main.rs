c3319e79cdca389d5ad866cccb0be8b5156eac94
Reinhard Bronner <reinhard_bronner@t-online.de>
2025-05-20 18:25:11 +0200

    add files-first flag

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use sonic_rs::{JsonContainerTrait, JsonValueTrait};
 use std::{fs::File, io::BufReader, path::PathBuf};
+use thiserror::Error;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -14,16 +15,20 @@ struct Args {
 
     #[arg(short = 'N', long)]
     no_dirs: bool,
+
+    #[arg(short = 'F', long)]
+    files_first: bool,
 }
 fn get_default_config_root() -> PathBuf {
     dirs::config_dir().expect("No config path").join("Code")
 }
 
-fn main() {
+fn main() -> Result<(), LibError> {
     let Args {
         config_root,
         no_files,
         no_dirs,
+        files_first,
     } = Args::parse();
     let config_root = config_root.unwrap_or_else(get_default_config_root);
     let storage_path = config_root.join("User/globalStorage/storage.json");
@@ -32,62 +37,105 @@ fn main() {
 
     let value: sonic_rs::Value = sonic_rs::from_reader(reader).unwrap();
     let items = value
-        .as_object()
-        .unwrap()
-        .get(&"lastKnownMenubarData")
-        .unwrap()
-        .as_object()
-        .unwrap()
-        .get(&"menus")
-        .unwrap()
-        .as_object()
-        .unwrap()
-        .get(&"File")
-        .unwrap()
-        .as_object()
-        .unwrap()
-        .get(&"items")
-        .unwrap()
+        .as_object_get("lastKnownMenubarData")?
+        .as_object_get("menus")?
+        .as_object_get("File")?
+        .as_object_get("items")?
         .as_array()
-        .unwrap();
+        .ok_or(LibError::FailedUseAsArray)?;
     let recent = items
         .iter()
         .find(|item| {
-            item.get("id").and_then(|s| s.as_str()) == Some("submenuitem.MenubarRecentMenu")
+            let Ok(id) = item.as_object_get("id") else {
+                return false;
+            };
+            let Some(id) = id.as_str() else {
+                return false;
+            };
+            id == "submenuitem.MenubarRecentMenu"
         })
         .unwrap();
     let uris = recent
-        .as_object()
-        .unwrap()
-        .get(&"submenu")
-        .unwrap()
-        .as_object()
-        .unwrap()
-        .get(&"items")
-        .unwrap()
+        .as_object_get("submenu")?
+        .as_object_get("items")?
         .as_array()
         .unwrap()
         .iter()
-        .filter(|item| {
-            let id = item.get("id").and_then(|s| s.as_str());
-            (!no_files && id == Some("openRecentFolder")
-                || !no_dirs && id == Some("openRecentFile"))
-                && item.get("enabled").and_then(|s| s.as_bool()) == Some(true)
-        })
-        .map(|item| {
-            item.as_object()
-                .unwrap()
-                .get(&"uri")
-                .unwrap()
-                .as_object()
-                .unwrap()
-                .get(&"path")
-                .unwrap()
-                .as_str()
-                .unwrap()
+        .filter_map(|item| {
+            let id = item.as_object_get("id").ok()?.as_str()?;
+            if (no_files || id != "openRecentFile") && (no_dirs || id != "openRecentFolder") {
+                return None;
+            }
+            if !item.get("enabled").and_then(|s| s.as_bool())? {
+                return None;
+            }
+            let val = item
+                .as_object_get("uri")
+                .ok()?
+                .as_object_get("path")
+                .ok()?
+                .as_str()?;
+            let t = match id {
+                "openRecentFile" => RecentEntryType::File,
+                "openRecentFolder" => RecentEntryType::Dir,
+                _ => unreachable!(),
+            };
+            Some(RecentEntry { t, val })
         })
         .collect::<Vec<_>>();
-    let mut out = uris.join("\n");
+
+    let mut out = if files_first {
+        let (files, dirs): (Vec<_>, Vec<_>) =
+            uris.iter().partition(|e| e.t == RecentEntryType::File);
+        files
+            .into_iter()
+            .chain(dirs)
+            .map(|e| e.val)
+            .collect::<Vec<_>>()
+            .join("\n")
+    } else {
+        uris.into_iter()
+            .map(|e| e.val)
+            .collect::<Vec<_>>()
+            .join("\n")
+    };
     out.push('\0');
     println!("{out}");
+    Ok(())
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum RecentEntryType {
+    File,
+    Dir,
+}
+
+#[derive(Debug, Clone)]
+struct RecentEntry<'a> {
+    t: RecentEntryType,
+    val: &'a str,
+}
+trait SonicRsValueExtensions {
+    fn as_object_get<'a>(&'a self, key: &str) -> Result<&'a sonic_rs::Value, LibError>;
+}
+
+impl SonicRsValueExtensions for sonic_rs::Value {
+    fn as_object_get<'a>(&'a self, key: &str) -> Result<&'a sonic_rs::Value, LibError> {
+        let res = self
+            .as_object()
+            .ok_or(LibError::FailedUseAsObject)?
+            .get(&key)
+            .ok_or(LibError::FailedGettingKey(key.to_owned()))?;
+        Ok(res)
+    }
+}
+
+#[derive(Debug, Error)]
+enum LibError {
+    #[error("Failed to use value as object.")]
+    FailedUseAsObject,
+    #[error("Failed to use value as array.")]
+    FailedUseAsArray,
+    #[error("Couldn't get value in object. Key: {}", .0)]
+    FailedGettingKey(String),
 }
